bc74b29a830024bcd0d1d12f649a1bb36848b373
jayphodges <jayphodges@gmail.com>
2017-11-01 11:31:20 -0600

    Working through best buy API

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
 
   def index
-  end
+    @conn = Faraday.new(url: "https://api.bestbuy.com/v1/") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+    reply = @conn.get("stores((area(80202,25)))?&format=json&apiKey=7a3heu7emrjz6qbsugmepbv6")
+
+    binding.pry
+    end
 
 end
