f88953a060513d34e59ac3687cb79d74ac8c9379
jayphodges <jayphodges@gmail.com>
2017-11-01 10:56:42 -0600

    Finished implementing API

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,11 +9,17 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-    render json: Item.create(item_params)
+    render json: Item.create(item_params), status: 204
   end
 
   def destroy
-
+    item = Item.find(params[:id])
+    if item
+      item.destroy
+      render json: item, status: 204
+    else
+      render json: item, status: 404
+    end
   end
 
   private
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -13,7 +13,7 @@ describe "Items API" do
 
     get "/api/v1/items"
     items = JSON.parse(response.body)
-    binding.pry
+
     expect(response.status).to eq(200)
     expect(items.count).to eq(3)
   end
@@ -30,28 +30,28 @@ describe "Items API" do
     expect(reply["name"]).to eq(item.name)
     expect(reply["description"]).to eq(item.description)
   end
-  #
-  # # When I send a DELETE request to /api/v1/items/1 I receive a 204 JSON response
-  # # if the record is successfully deleted
-  # it "responds to a DELETE request to /api/v1/items/:id it responds after deleting the item" do
-  #   item = create(:item)
-  #
-  #   delete "api/v1/items/#{item.id}"
-  #   reply = JSON.parse(result.body)
-  #
-  #   expect(response.status).to eq(204)
-  # end
-  #
+
+  # When I send a DELETE request to /api/v1/items/1 I receive a 204 JSON response
+  # if the record is successfully deleted
+  it "responds to a DELETE request to /api/v1/items/:id it responds after deleting the item" do
+    item = Item.create(name: "Item1", description: "Description1", image_url: "http://via.placeholder.com/350x350")
+
+    delete "api/v1/items/#{item.id}"
+
+    expect(response.status).to eq(204)
+    expect(Item.count).to eq(0)
+  end
+
   # When I send a POST request to /api/v1/items with a name, description, and image_url
   # I receive a 201 JSON response if the record is successfully created And I receive a
   # JSON response containing the id, name, description, and image_url but not the created_at or updated_at
   it "responds to a POST request with a 201 status and returns the item" do
     params = {:item => {name: "Item1", description: "Description1", image_url: "http://via.placeholder.com/350x350"}}
 
-    # post "api/v1/items", params
+    post "api/v1/items", params
 
-    reply = JSON.parse(result.body)
-    binding.pry
     expect(response.status).to eq(204)
+    expect(Item.last.name).to eq("Item1")
+
   end
 end
