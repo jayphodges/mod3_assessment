00ae79754a4d5a829be795834b81661b1775fcb5
jayphodges <jayphodges@gmail.com>
2017-11-01 10:14:51 -0600

    Experienced an issue getting API setup, moving past now

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -11,26 +11,25 @@ describe "Items API" do
     Item.create(name: "Item2", description: "Description2", image_url: "http://via.placeholder.com/350x350")
     Item.create(name: "Item3", description: "Description3", image_url: "http://via.placeholder.com/350x350")
 
-    get "http://localhost:3000/api/v1/items"
-    binding.pry
-    items = JSON.parse(result.body)
+    get "/api/v1/items"
+    items = JSON.parse(response.body)
 
-    expect(response).to be_success
+    expect(response.status).to eq(200)
     expect(items.count).to eq(3)
   end
-  #
-  # # When I send a GET request to /api/v1/items/1 I receive a 200 JSON response containing
-  # #  the id, name, description, and image_url but not the created_at or updated_at
-  # it "responds to a GET request to /api/v1/items/:id and doesn't return created/updated_at" do
-  #   item = create(:item)
-  #
-  #   get "api/v1/items/#{item.id}"
-  #   item = JSON.parse(result.body)
-  #
-  #   expect(response).to be_success
-  #   expect(item.count).to eq(1)
-  #   expect(item.name).to eq(item)
-  # end
+
+  # When I send a GET request to /api/v1/items/1 I receive a 200 JSON response containing
+  #  the id, name, description, and image_url but not the created_at or updated_at
+  it "responds to a GET request to /api/v1/items/:id and doesn't return created/updated_at" do
+    item = Item.create(name: "Item1", description: "Description1", image_url: "http://via.placeholder.com/350x350")
+
+    get "api/v1/items/#{item.id}"
+    item = JSON.parse(result.body)
+
+    expect(response).to be_success
+    expect(item.count).to eq(1)
+    expect(item.name).to eq(item)
+  end
   #
   # # When I send a DELETE request to /api/v1/items/1 I receive a 204 JSON response
   # # if the record is successfully deleted
